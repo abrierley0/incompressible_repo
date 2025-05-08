ea40ec50f904b33ac2a08068b5b21a6c9624991a
abrierley0 <brierley.ajb@gmail.com>
2025-05-08 14:17:52 +0100

    Added IC and BCs, main loop and NS differencing

--- pressure_poisson/pressure_poisson.m
+++ pressure_poisson/pressure_poisson.m
@@ -10,17 +10,19 @@
 % Centre for Computational Engineering Sciences (CES)
 % Cranfield, Bedfordshire, MK43 0AL, UK
 
+% Written in MATLAB programming language
+
 clc
 clear all
 close all
 format long
 
-%--------------------------------
+%=============================
 % COMPUTATIONAL DOMAIN
-%--------------------------------     
+%============================   
 %                                       NORTH
 %                                     dp/dy = 0
-%                   i = 1, j = jmax                   i = imax, j = jmax
+%                   (i = 1, j = jmax)                   (i = imax, j = jmax)
 %                       D-------------------------------------------C
 %                       |*******************************************|
 %                       |*    INTERNAL DOMAIN                      *|
@@ -30,43 +32,52 @@ format long
 %                       |*                                         *|
 %                       |*******************************************|
 %                       A-------------------------------------------B
-%                   i = 1,                                i = imax,
-%                   j = 1            -dp/dy = 0           j = jmax
+%                   (i = 1,                                (i = imax,
+%                   j = 1)            -dp/dy = 0           j = jmax)
 %                                       SOUTH
 
 %=========================
 % INPUT VALUES
 %=========================
+Re = 100   % Reynolds number
+
 L = 2;     % Length of channel [m]
 H = 0.75;  % Height of channel [m]
 W = 0.01;  % Width of channel  [m]
 
 imax = 100;
 jmax = 40;
+nmax = 40000;  % Max. time steps
 
-% NOTE: remember to subtract 1 for the spaces
-dx = L / (imax-1);
+dx = L / (imax-1);  % NOTE: remember to subtract 1 for the spaces
 dy = H / (jmax-1);
 
-% Fluid properties - water
 rho = 997;       % Density [kg/m^3]
 mu = 1.0016;     % Dynamic viscosity [Pa*s]
+nu = mu / rho;
+
+ua = 1;  % Initial velocity [m/s]
 
 
 %==================
 % INITIALISE
 %==================
-X = zeros(imax,jmax);  % NOTE: X and Y are capitalised
+X = zeros(imax,jmax);  % NOTE: X and Y are uppercase
 Y = zeros(imax,jmax);
 
-u = zeros(imax,imax);
+u = zeros(imax,jmax);  % New
 v = zeros(imax,jmax);
+p = zeros(imax,jmax);
+
+un = zeros(imax,jmax);  % Old
+vn = zeros(imax,jmax);
+pn = zeros(imax,jmax);
 
 
 %=======================
 % GRID DEFINITION
 %=======================
-% NOTE: x and y are lower case
+% NOTE: x and y are lowercase
 y = 0.0;
 for j = 1:jmax
     x = 0.0;
@@ -78,4 +89,81 @@ for j = 1:jmax
     y = y + dy;
 end
 
+%==================================
+% INITIAL AND BOUNDARY CONDITIONS
+%==================================
+% INITIAL CONDITIONS
+for i = 1:imax
+    for j = 2:jmax-1
+        u(i,j) = ua;
+        v(i,j) = 0.0;
+        p(i,j) = 0.0;
+    end 
+end
 
+% WEST (INLET) BOUNDARY CONDITION
+for i = 1
+    for j = 2:(jmax-1)
+        u(i,j) = ua;
+        v(i,j) = 0.0;
+    end
+end
+
+% EAST (OUTLET) BOUNDARY CONDITION
+for i = imax
+    for j = 2:(jmax-1)
+        u(i,j) = 0.0;
+        v(i,j) = 0.0;
+    end
+end
+
+% NORTH (UPPER WALL) BOUNDARY CONDITION
+for j = jmax
+    for i = 2:(imax-1)
+        u(i,j) = 0.0;
+        v(i,j) = 0.0;
+    end
+end
+
+% SOUTH (LOWER WALL) BOUNDARY CONDITION
+for j = 1
+    for i = 2:(imax-1)
+        u(i,j) = 0.0;
+        v(i,j) = 0.0;
+    end
+end
+
+
+%============================
+% START MAIN TIME LOOP
+%============================
+
+for n = 1:nmax
+
+    % ASSIGN NEW VALUES TO THE OLD VALUES
+    for i = 1:imax
+        for j = 1:jmax
+            un(i,j) = u(i,j);
+            vn(i,j) = v(i,j);
+            pn(i,j) = p(i,j);
+        end
+    end
+
+    %==============================================
+    % SOLVE THE NAVIER-STOKES MOMENTUM EQUATIONS
+    %==============================================
+
+    % Compute viscous terms with central diff.
+    d2udx2 = (un(i+1,j)-2*un(i,j)+un(i-1,j))/(dx*dx);
+    d2udy2 = (un(i,j+1)-2*un(i,j)+un(i,j-1))/(dy*dy);
+    LaplacianU = d2udx2 + d2udy2;
+
+    d2vdx2 = (vn(i+1,j)-2*vn(i,j)+vn(i-1,j))/(dx*dx);
+    d2vdy2 = (vn(i,j+1)-2*vn(i,j)+vn(i,j-1))/(dy*dy);
+    LaplacianV = d2vdx2 + d2vdy2;
+
+    % Compute 
+
+
+
+end
--- pressure_poisson/pressure_poisson.md
+++ pressure_poisson/pressure_poisson.md
@@ -1,7 +1,7 @@
 Pressure-Poisson Solver
 =======================
 
-The method of Harlow & Welch, Los Alamos (1951). This was one of the earliest solvers and one of the most efficient. The essence is to take the divergence of the incompressible Navier-Stokes equation, resulting in cancellation of unsteady, gravity, and viscous terms, leaving
+This is the method of Harlow & Welch, Los Alamos (1951). This was one of the earliest solvers and one of the most efficient. The essential question is how can we solve the incompressible Navier-Stokes if we do not know the pressure field? The essence is to take the divergence of the incompressible Navier-Stokes equation, resulting in cancellation of unsteady, gravity, and viscous terms, leaving
 
 $$\nabla \cdot (\mathbf{u} \cdot \nabla) \mathbf{u} = - \frac{1}{\rho} \nabla \cdot \nabla p $$
 
@@ -9,13 +9,27 @@ Then there are two vector identities used to change the LHS and you multiply by 
 
 $$ \nabla ^2 p = - \rho \left[(\nabla \otimes \mathbf{u}) \cdot \cdot (\nabla \otimes \mathbf{u})\right]$$
 
+Giving us an equation for the pressure. It can be seen that there is a strong dependence on the velocity-gradient tensors. **Note**: exactly why this equation can be used should be thought about.
+
 Expanding for the scalar form of the Pressure-Poisson equation,
 
 $$ \frac{\partial^2 p}{\partial x^2} + \frac{\partial^2 p}{\partial y^2} + \frac{\partial^2 p}{\partial z^2} = \rho \left[ \left(\frac{\partial u}{\partial x}\right)^2 + \left(\frac{\partial v}{\partial y}\right)^2 + \left(\frac{\partial w}{\partial z}\right)^2 + 2 \left(\frac{\partial v}{\partial x} \cdot \frac{\partial u}{\partial y} + \frac{\partial w}{\partial x} \cdot \frac{\partial u}{\partial z} + \frac{\partial w}{\partial y} \cdot \frac{\partial v}{\partial z}\right) \right]$$
 
-We need a numerical solution for this equation!!
+We need a numerical solution for this equation!! There is then an algorithm with five equations involved - the continuity equation, the x-Navier-Stokes, y-Navier-Stokes, z- Navier-Stokes, and the above Pressure-Poisson. An algorithm must be developed to solve these. You can use the Gauss-Seidel Successive Over-Relaxation approach or also known as the point SOR approach. For the derivative terms, one can use appropriate finite differencing. A five-point discretisation stencil is used with this.
 
-For simplicity's sake, reduce the problem to two dimensions.
+Let's write the system of equations in scalar form for two dimensions.
 
-First we need to develop an algorithm to solve.
+1. $$\frac{\partial u}{\partial x} + \frac{\partial v}{\partial y} = 0$$
 
+2. $$\frac{\partial u}{\partial t} + \left(u \frac{\partial u}{\partial x} + v \frac{\partial u}{\partial y}\right) = g_x - \frac{1}{\rho} \frac{\partial p}{\partial x} + \nu \left(\frac{\partial^2 u}{\partial x^2} + \frac{\partial^2 u}{\partial y^2}\right)$$
+
+3. $$\frac{\partial v}{\partial t} + \left(u \frac{\partial v}{\partial x} + v \frac{\partial v}{\partial y}\right) = g_y - \frac{1}{\rho} \frac{\partial p}{\partial y} + \nu \left(\frac{\partial^2 v}{\partial x^2} + \frac{\partial^2 v}{\partial y^2}\right)$$
+
+4. $$\frac{\partial^2 p}{\partial x^2} + \frac{\partial^2 p}{\partial y^2} = -\rho \left[\left(\frac{\partial u}{\partial x}\right)^2 +\left(\frac{\partial v}{\partial y}\right)^2 + 2 \left(\frac{\partial v}{\partial x} \cdot \frac{\partial u}{\partial y}\right) \right]$$
+
+Questions
+---------
+
+- Why can this equation for the pressure be used?
+- Is it an approximation?
+- Why do the boundary conditions use (u,v,p) but the initial conditions (un,vn,pn)?
