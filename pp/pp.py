9d264c75858e1833b57909a3cf90fd49e1a82adf
abrierley0 <brierley.ajb@gmail.com>
2025-12-19 14:34:06 +0000

    Added x momentum equation

--- pp/pp.py
+++ pp/pp.py
@@ -26,6 +26,8 @@ dx = L/(nx-1)
 dy = H/(ny-1)
 
 # Fluid
+rho = 1.0
+nu = 0.1
 
 
 #####################
@@ -38,6 +40,13 @@ u = np.zeros((nx,ny))
 v = np.zeros((nx,ny))
 w = np.zeros((nx,ny))
 p = np.zeros((nx,ny))
+
+Xn = np.zeros((nx,ny))
+Yn = np.zeros((nx,ny))
+un = np.zeros((nx,ny))
+vn = np.zeros((nx,ny))
+wn = np.zeros((nx,ny))
+pn = np.zeros((nx,ny))
 
 
 ###############################
@@ -100,6 +109,48 @@ v[0,0] = (v[1,0] + v[0,1])/2.0
 v[nx-1,0] = (v[nx-2,0] + v[nx-1,1])/2.0
 v[0,ny-1] = (v[0,ny-1] + v[1,ny-1])/2.0
 v[nx-1,ny-1] = (v[nx-1,ny-1] + v[nx-1,ny-1])/2.0
+
+####################
+# SOLVE CONTINUITY #
+####################
+
+# Check continuity
+        
+
+#################################
+# SOLVE THE X MOMENTUM EQUATION #
+#################################
+
+for i in range(1,nx):
+    for j in range(1,ny):
+        dudx = (un[i,j] - un[i,j-1])/dx
+        dudy = (un[i,j] - un[i,j-1])/dy
+        dvdx = (vn[i,j] - vn[i,j-1])/dx
+        dvdy = (vn[i,j] - vn[i,j-1])/dy
+        d2udx2 = (un[i+1,j] - 2*un[i,j] + un[i-1,j])/dx^2
+        d2udy2 = (un[i+1,j] - 2*un[i,j] + un[i-1,j])/dy^2
+        d2vdx2 = (vn[i+1,j] - 2*vn[i,j] + vn[i-1,j])/dx^2
+        d2vdy2 = (vn[i+1,j] - 2*vn[i,j] + vn[i-1,j])/dy^2
+        dpdx = (pn[i,j] - pn[i-1,j])/dx
+        dpdy = (pn[i,j] - pn[i-1,j])/dy
+        RHSU = dpdx/rho + un[i,j]*(dudx) + vn[i,j]*(dvdy) - nu*(d2udx2 + d2udy2)
+        RHSV = dpdy/rho + un[i,j]*(dvdx) + vn[i,j]*(dvdy) - nu*(d2vdx2 + d2vdy2)
+        u[i,j] = un[i,j] - dt*RHSU
+        v[i,j] = vn[i,j] - dt*RHSV
+
+
+
+##########################
+# SCALAR FORM MOMENTUM Y #
+##########################
+
+##########################
+# SCALAR FORM MOMENTUM Z #
+##########################
+
+################################
+# SCALAR FORM PRESSURE-POISSON #
+################################
 
 print(f"u[nx-1,1]: {u[nx-1,1]}")
 
